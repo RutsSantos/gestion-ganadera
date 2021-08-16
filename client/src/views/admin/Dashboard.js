9ff5726835b13b15377274d3f767241f3e9eb6f8
RutsSantos <rsantos@intellisys.com.do>
2021-08-16 18:59:20 -0400

    remove unused blocks

--- client/src/views/admin/Dashboard.js
+++ client/src/views/admin/Dashboard.js
@@ -18,14 +18,14 @@ export default function Dashboard() {
           <CardBarChart />
         </div>
       </div>
-      <div className="flex flex-wrap mt-4">
+      {/* <div className="flex flex-wrap mt-4">
         <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
           <CardPageVisits />
         </div>
         <div className="w-full xl:w-4/12 px-4">
           <CardSocialTraffic />
         </div>
-      </div>
+      </div> */}
     </>
   );
 }
