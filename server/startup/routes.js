1d45c1da55425afd0777d0f0965bf656a33096be
RutsSantos <rsantos@intellisys.com.do>
2021-08-17 17:52:47 -0400

    Create controller

--- server/startup/routes.js
+++ server/startup/routes.js
@@ -1,8 +1,9 @@
 const animales = require('../controllers/animales');
 const estado_animal = require('../controllers/estado_animal');
-
+const empleados = require('../controllers/empleados');
 
 module.exports = function(app){
   app.use('/animales', animales);
   app.use('/estado_animal', estado_animal);
+  app.use('/empleados', empleados);
 }
