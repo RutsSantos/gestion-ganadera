fb4eedc09df8b3c387bde7388a9da62a5d8c7911
RutsSantos <rsantos@intellisys.com.do>
2021-08-16 23:19:01 -0400

    Reusing CardTable component, create animal inventory and databse models

--- client/src/views/business/Inventory.js
+++ client/src/views/business/Inventory.js
@@ -1,19 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 // components
 import CardTable from "components/Cards/CardTable.js";
 
 export default function Inventory() {
+  const [data, setData] = useState([]);
+  const [estado_animal, setEstado] = useState([]);
+
+  useEffect(() => {
+    async function fetchMyAPI() {
+      const response = await axios.get('http://localhost:3200/animales')
+      setData(response.data)
+      const response2 = await axios.get('http://localhost:3200/estado_animal')
+      setEstado(response2.data)
+    }
+    fetchMyAPI()
+
+
+
+  }, [])
+  data.map(item => {
+    estado_animal.map(estado => item["id_estado_animal"] === estado["id_estado_animal"] && (item["id_estado_animal"] = estado["descripcion"]));
+    const date = new Date(item["nacimiento"]);
+    item["nacimiento"] = date.toLocaleString()
+  });
+  let headers = data.length > 0 ? Object.keys(data[0]) : [];
+  headers[4] = "Estado animal"
   return (
     <>
-      <div className="flex flex-wrap mt-4">
+      {data.length > 0 && <div className="flex flex-wrap mt-4">
         <div className="w-full mb-12 px-4">
-          <CardTable />
+          <CardTable title="Inventario De Animales" data={data} headers={headers} />
         </div>
-        <div className="w-full mb-12 px-4">
-          <CardTable color="dark" />
-        </div>
-      </div>
+      </div>}
     </>
   );
 }
--- server/startup/routes.js
+++ server/startup/routes.js
@@ -1,5 +1,8 @@
 const animales = require('../controllers/animales');
+const estado_animal = require('../controllers/estado_animal');
+
 
 module.exports = function(app){
   app.use('/animales', animales);
+  app.use('/estado_animal', estado_animal);
 }
