7ff56f09e3456e392c46ccd17d42e1f709d2da5a
RutsSantos <rsantos@intellisys.com.do>
2021-08-17 08:13:10 -0400

    Change styles

--- client/src/components/Footers/FooterAdmin.js
+++ client/src/components/Footers/FooterAdmin.js
@@ -4,7 +4,7 @@ export default function FooterAdmin() {
   return (
     <>
       <footer className="block py-4">
-        <div className="container mx-auto px-4">
+        <div className="container mx-auto px-4" style={{position: "fixed", bottom: "12px"}}>
           <hr className="mb-4 border-b-1 border-blueGray-200" />
           <div className="flex flex-wrap items-center md:justify-between justify-center">
             <div className="w-full md:w-4/12 px-4">
--- client/src/views/business/Inventory.js
+++ client/src/views/business/Inventory.js
@@ -29,9 +29,18 @@ export default function Inventory() {
   headers[4] = "Estado animal"
   return (
     <>
+      <div className="text-center flex justify-between">
+        <h6 className="text-white text-xl font-bold">Inventario De Animales</h6>
+        <button
+          className="bg-lightBlue-500 text-white active:bg-lightBlue-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
+          type="button"
+        >
+          Añadir
+        </button>
+      </div>
       {data.length > 0 && <div className="flex flex-wrap mt-4">
         <div className="w-full mb-12 px-4">
-          <CardTable title="Inventario De Animales" data={data} headers={headers} />
+          <CardTable data={data} headers={headers} />
         </div>
       </div>}
     </>
